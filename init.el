0dc1baf59419b9b5f6cccd00112f921c51b3e7ba
Alex Podaras <apodaras@gmail.com>
2014-03-16 18:02:18 +0200

    Change font

--- init.el
+++ init.el
@@ -6,3 +6,5 @@
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode blink-cursor-mode))
   (when (fboundp mode) (funcall mode -1)))
 
+;; Font
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-12"))
