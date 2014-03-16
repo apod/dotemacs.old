b78de6b1d4688717b380b93f2e8e7a736a078c6a
Alex Podaras <apodaras@gmail.com>
2014-03-16 16:02:33 +0200

    Turn off menu, toolbar and scrollbar

--- init.el
+++ init.el
@@ -1,1 +1,7 @@
 ;;; init.el
+
+;;; Interface
+
+;; Turn off menu-bar, tool-bar, scroll-bar modes
+(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+  (when (fboundp mode) (funcall mode -1)))
