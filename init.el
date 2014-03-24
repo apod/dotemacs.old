4a585c94703d01f08d461798032da96410acdd9e
Alex Podaras <apodaras@gmail.com>
2014-03-25 00:03:27 +0200

    Swap default isearch keybindings with the regexp equivalents

--- init.el
+++ init.el
@@ -112,6 +112,15 @@
 (setq-default history-length 1000)
 (savehist-mode t)
 
+;;; Keybindings
+
+;; Swap isearch with the regexp equivalents
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+
 ;;; Interface
 
 ;; Highlight current line
