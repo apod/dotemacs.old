98b885e076b077087cbcb76617d99da108de3faf
Alex Podaras <apodaras@gmail.com>
2014-03-26 00:02:25 +0200

    Split core configuration

---
+++ config/ap-core.el
@@ -0,0 +1,59 @@
+;;; ap-core.el
+;;; Core
+
+;; Make sure the coding system is utf-8
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Store backup files
+(setq backup-directory-alist
+      `((".*" . ,ap-backup-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "\\1" ap-backup-directory) t)))
+(setq auto-save-list-file-prefix
+      (expand-file-name ".saves-"
+                        (expand-file-name "auto-save-list" ap-cache-directory)))
+
+;; Revert buffer if the file was changed
+(global-auto-revert-mode t)
+
+;; Inhibit startup screen
+(setq inhibit-startup-screen t)
+
+;; Meaningful names for buffers with the same name
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward
+      uniquify-separator "/"
+      uniquify-ignore-buffers-re "^\\*" ; ignore special buffers
+      uniquify-after-kill-buffer-p t)
+
+;; Save last cursor position
+(require 'saveplace)
+(setq save-place-file (expand-file-name "saveplace" ap-cache-directory))
+(setq-default save-place t)
+
+;; Save minibuffer history
+(require 'savehist)
+(setq savehist-file (expand-file-name "savehist" ap-cache-directory)
+      savehist-additional-variables '(search ring regexp-search-ring)
+      savehist-autosave-interval 60)
+(setq-default history-length 1000)
+(savehist-mode t)
+
+;; Scrolling
+(setq scroll-margin 3
+      scroll-conservatively 100000
+      scroll-preserve-screen-position t)
+
+;; Lower the delay for displaying keystrokes in minibuffer
+(setq echo-keystrokes 0.1)
+
+;; Delete selection on key press
+(delete-selection-mode t)
+
+;; Answer questions with y or n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(provide 'ap-core)
--- init.el
+++ init.el
@@ -28,6 +28,7 @@
 
 ;;; Modules
 (require 'ap-packages)
+(require 'ap-core)
 (require 'ap-appearance)
 (require 'ap-mode-line)
 (require 'ap-whitespace)
@@ -35,67 +36,7 @@
 (require 'ap-ido)
 (require 'ap-smex)
 
-;;; Core
-
 ;; File to store customization information
 (setq custom-file (expand-file-name "customizations.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
-
-;; Store backup files on .cache directory
-(setq backup-directory-alist
-      `((".*" . ,ap-backup-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,(expand-file-name "\\1" ap-backup-directory) t)))
-(setq auto-save-list-file-prefix
-      (expand-file-name ".saves-"
-                        (expand-file-name "auto-save-list" ap-cache-directory)))
-
-;; Revert buffer if the file was changed
-(global-auto-revert-mode t)
-
-;; Make sure the coding system is utf-8
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-
-;; Answer questions with y or n
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Meaningful names for buffers with the same name
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward
-      uniquify-separator "/"
-      uniquify-ignore-buffers-re "^\\*" ; ignore special buffers
-      uniquify-after-kill-buffer-p t)
-
-
-;; Delete selection on key press
-(delete-selection-mode t)
-
-;; Lower the delay for displaying keystrokes in minibuffer
-(setq echo-keystrokes 0.1)
-
-;; Scrolling
-(setq scroll-margin 3
-      scroll-conservatively 100000
-      scroll-preserve-screen-position t)
-
-;; Save last cursor position
-(require 'saveplace)
-(setq save-place-file (expand-file-name "saveplace" ap-cache-directory))
-(setq-default save-place t)
-
-;; Save minibuffer history
-(require 'savehist)
-(setq savehist-file (expand-file-name "savehist" ap-cache-directory)
-      savehist-additional-variables '(search ring regexp-search-ring)
-      savehist-autosave-interval 60)
-(setq-default history-length 1000)
-(savehist-mode t)
-
-;;; Interface
-
-;; Inhibit startup screen
-(setq inhibit-startup-screen t)
