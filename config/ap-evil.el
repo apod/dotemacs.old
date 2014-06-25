482a9196534c6be79a714df3e4ac3ba263646ad3
Alex Podaras <alex@kinems.com>
2014-06-25 11:43:57 +0300

    Add evil-visualstar

--- config/ap-evil.el
+++ config/ap-evil.el
@@ -3,9 +3,11 @@
 
 (ap-ensure-package 'evil)
 (ap-ensure-package 'evil-surround)
+(ap-ensure-package 'evil-visualstar)
 
 (require 'evil)
 (require 'evil-surround)
+(require 'evil-visualstar)
 
 (evil-mode t)
 (global-evil-surround-mode t)
