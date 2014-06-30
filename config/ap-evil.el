91505e5e27085aa4e7c40b9f85380f9903cb801c
Alex Podaras <apodaras@gmail.com>
2014-07-01 00:39:35 +0300

    Add [<space> ]<space> mappings for adding blank lines

--- config/ap-evil.el
+++ config/ap-evil.el
@@ -12,4 +12,23 @@
 (evil-mode t)
 (global-evil-surround-mode t)
 
+
+;;; Keybindings
+
+;; Partial functionality from vim-unimpaired
+(defun ap-blank-line-above ()
+  (interactive)
+  (save-excursion 
+    (beginning-of-line)
+    (newline)))
+
+(defun ap-blank-line-below ()
+  (interactive)
+  (save-excursion 
+    (end-of-line)
+    (newline)))
+
+(define-key evil-normal-state-map "] " 'ap-blank-line-below) ; ]<space>
+(define-key evil-normal-state-map "[ " 'ap-blank-line-above) ; [<space>
+
 (provide 'ap-evil)
