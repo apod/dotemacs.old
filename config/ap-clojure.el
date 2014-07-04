a4260cc35f624db9fcb7b0e8d0419038d2bda08c
Alex Podaras <apodaras@gmail.com>
2014-07-05 00:15:25 +0300

    Configure cider-repl

--- config/ap-clojure.el
+++ config/ap-clojure.el
@@ -5,7 +5,17 @@
 (ap-ensure-package 'clojure-mode)
 (ap-ensure-package 'cider)
 
+;; Hide cider REPL buffers
 (setq nrepl-hide-special-buffers t)
+
+;; Prevent the auto-display of the REPL buffer in a separate window after connection is established
+(setq cider-repl-pop-to-buffer-on-connect nil)
+
+;; Store cider REPL history
+(setq cider-repl-history-file (expand-file-name "cider-history" ap-cache-directory))
+
+;; Run cider REPL on emacs mode
+(add-to-list 'evil-emacs-state-modes 'cider-repl-mode)
 
 (add-hook 'clojure-mode-hook (lambda ()
                                (rainbow-delimiters-mode t)
@@ -17,7 +27,6 @@
                                   (rainbow-delimiters-mode t)
                                   (smartparens-strict-mode t)))
 
-(add-to-list 'evil-emacs-state-modes 'cider-repl-mode)
 
 ;; Make cider preceding point commands work on point when using evil's normal mode
 (ap-advice-commands "evil-preceding-point"
