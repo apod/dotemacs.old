0786536cab2f057100c45ba6aa6cf859d0443a63
Alex Podaras <apodaras@gmail.com>
2014-08-14 23:41:02 +0300

    Customize whitespace mode

--- config/ap-whitespace.el
+++ config/ap-whitespace.el
@@ -4,7 +4,17 @@
 ;; Don't use tab character for indentation
 (setq-default indent-tabs-mode nil)
 
+;; Whitespace mode setup
+(setq whitespace-display-mappings '((newline-mark 10 [172 10])
+                                    (space-mark 32 [183] [46])
+                                    (tab-mark 9 [187 9] [92 9])))
+
 ;; Show trailing whitespace
-;; (setq-default show-trailing-whitespace t)
+(define-minor-mode apod-show-trailing-whitespace-mode
+  "Enables `show-trailing-whitespace'."
+  :lighter nil
+  (progn (setq show-trailing-whitespace apod-show-trailing-whitespace-mode)))
+
+(add-hook 'prog-mode-hook 'apod-show-trailing-whitespace-mode)
 
 (provide 'ap-whitespace)
