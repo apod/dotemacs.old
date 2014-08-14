38384d5c9e260fcb05828235c539c5f3db17a9ae
Alex Podaras <apodaras@gmail.com>
2014-08-15 01:21:27 +0300

    Add narrow-or-widen-dwim interactive function

--- config/ap-evil.el
+++ config/ap-evil.el
@@ -20,10 +20,27 @@
 ;; evil-leader
 (global-evil-leader-mode t)
 
+;; http://endlessparentheses.com/emacs-narrow-or-widen-dwim.html
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
+Intelligently means: region, subtree, or defun, whichever applies
+first.
+
+With prefix P, don't widen, just narrow even if buffer is already
+narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
+        (t (narrow-to-defun))))
+
 (evil-leader/set-leader "\\")
 (evil-leader/set-key "ol" 'linum-mode
                      "oh" 'whitespace-mode
-                     "\\" 'evil-buffer)
+                     "\\" 'evil-buffer
+                     "n"  'narrow-or-widen-dwim)
 
 
 (evil-mode t)
