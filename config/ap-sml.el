7e19eaa94f3df7eb5f783ae9e4edd60218c1d7fe
Alex Podaras <apodaras@gmail.com>
2014-10-01 14:07:32 +0300

    SML send line if there is no region

--- config/ap-sml.el
+++ config/ap-sml.el
@@ -3,4 +3,15 @@
 
 (ap-ensure-package 'sml-mode)
 
+(defun ap-sml-send-region-or-line ()
+  (interactive)
+  (if (use-region-p)
+      (sml-prog-proc-send-region (region-beginning)
+                                 (region-end))
+    (sml-prog-proc-send-region (line-beginning-position)
+                               (line-beginning-position 2))))
+
+(eval-after-load 'sml-mode
+  '(define-key sml-mode-map (kbd "C-c C-r") 'ap-sml-send-region-or-line))
+
 (provide 'ap-sml)
