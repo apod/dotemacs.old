895d06f7a60f266ed53fb5c9af876e1f09808890
Alex Podaras <apodaras@gmail.com>
2014-10-18 21:14:34 +0300

    Add a keybinding to kill sml process and load a the current file

--- config/ap-sml.el
+++ config/ap-sml.el
@@ -11,7 +11,18 @@
     (sml-prog-proc-send-region (line-beginning-position)
                                (line-beginning-position 2))))
 
+(defun ap-sml-kill-and-load-file (file)
+  (interactive
+   (list (or buffer-file-name
+             (read-file-name "File to load: " nil nil t))))
+  (when (processp (get-buffer-process "*sml*"))
+    (delete-process "*sml*"))
+  (sml-prog-proc-load-file file)
+  (other-window 1))
+
 (eval-after-load 'sml-mode
-  '(define-key sml-mode-map (kbd "C-c C-r") 'ap-sml-send-region-or-line))
+  '(progn
+     (define-key sml-mode-map (kbd "C-c C-r") 'ap-sml-send-region-or-line)
+     (define-key sml-mode-map (kbd "C-c C-k") 'ap-sml-kill-and-load-file)))
 
 (provide 'ap-sml)
