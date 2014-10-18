8dd5ec279173a4c2ca48cf63e3eae46c859e3749
Alex Podaras <apodaras@gmail.com>
2014-10-18 21:14:58 +0300

    Configure haskell mode

--- config/ap-haskell.el
+++ config/ap-haskell.el
@@ -7,20 +7,17 @@
 (eval-after-load 'haskell-mode
   '(progn
      (defun ap-haskell-mode-defaults ()
-       (subword-mode +1)
        (turn-on-haskell-doc-mode)
-       (turn-on-haskell-indentation))
+       (turn-on-haskell-indentation)
+       (interactive-haskell-mode t))
 
      (setq ap-haskell-mode-hook 'ap-haskell-mode-defaults)
+     (setq haskell-process-suggest-remove-import-lines t)
+     (setq haskell-process-auto-import-loaded-modules t)
+     (setq haskell-process-log t)
+     (setq haskell-process-type 'ghci)
 
      (add-hook 'haskell-mode-hook (lambda ()
                                     (run-hooks 'ap-haskell-mode-hook)))))
-
-;; http://comments.gmane.org/gmane.comp.lang.haskell.cafe/85859
-(defadvice inferior-haskell-load-file (after change-focus-after-load)
-  "Switch focus to inferior haskell process after loading buffer (C-c C-l)"
-  (other-window 1))
-
-(ad-activate 'inferior-haskell-load-file)
 
 (provide 'ap-haskell)
