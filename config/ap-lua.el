974b3a4c44015f19c4fef18adeb58aead03d393e
Alex Podaras <apodaras@gmail.com>
2014-12-16 19:56:02 -0500

    Add keybinding for lua-send-defun

--- config/ap-lua.el
+++ config/ap-lua.el
@@ -7,6 +7,7 @@
   '(progn
      (define-key lua-mode-map (kbd "C-c C-r") 'lua-send-region)
      (define-key lua-mode-map (kbd "C-c C-e") 'lua-send-current-line)
+     (define-key lua-mode-map (kbd "C-M-x")   'lua-send-defun)
 
      (setq lua-indent-level 2)))
 
